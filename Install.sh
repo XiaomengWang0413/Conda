10134eac12ec4e22ed572a33d4dc257b4340cb1f
Xiaomeng Wang <50069873+XiaomengWang0413@users.noreply.github.com>
2024-03-26 19:07:24 +0800

    Create Install.sh

---
+++ Install.sh
@@ -0,0 +1,5 @@
+#bin/bash
+ProxyError: Conda cannot proceed due to an error in your proxy configuration. Check for typos and other configuration errors in any ‘.netrc’ file in your home directory, any environment variables ending n ‘_PROXY’, and any other system-wide proxy configuration settings.
+
+.condarc
+##掉Proxy
